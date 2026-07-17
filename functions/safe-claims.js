6ba581a15bd26a12d6288ae4f6901f20e385f060
jepthe Nkwanmen <107397519+nosby2007@users.noreply.github.com>
2026-07-17 13:47:55 -0400

    Route courier claim export to hardened backend

--- functions/safe-claims.js
+++ functions/safe-claims.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const marketplace = require('./marketplace-v2');
+const marketplace = require('./marketplace-v3');
 
 module.exports = {
   claimDeliveryJob: marketplace.claimDeliveryJob
