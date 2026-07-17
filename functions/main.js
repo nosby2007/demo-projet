ec0e4f91713fc75b2766aeaf624925f7387a5534
jepthe Nkwanmen <107397519+nosby2007@users.noreply.github.com>
2026-07-17 14:06:49 -0400

    Compose idempotent checkout, tenant catalogue and role recovery

--- functions/main.js
+++ functions/main.js
@@ -2,10 +2,12 @@
 
 const marketplace = require('./marketplace-v3');
 const checkout = require('./checkout-v4');
+const catalog = require('./catalog-v4');
 const roleApproval = require('./role-approval');
 
 module.exports = {
   ...marketplace,
   ...checkout,
-  approveRoleRequest: roleApproval.approveRoleRequest
+  ...catalog,
+  ...roleApproval
 };
