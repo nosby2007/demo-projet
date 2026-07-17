b241afc50b66ddb0c8274f3283c4af2adb261a83
jepthe Nkwanmen <107397519+nosby2007@users.noreply.github.com>
2026-07-17 14:24:12 -0400

    Deploy catalogue v5 consistency controls

--- functions/main.js
+++ functions/main.js
@@ -2,7 +2,7 @@
 
 const marketplace = require('./marketplace-v3');
 const checkout = require('./checkout-v4');
-const catalog = require('./catalog-v4');
+const catalog = require('./catalog-v5');
 const roleApproval = require('./role-approval');
 
 module.exports = {
