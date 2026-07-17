642ff8c102a8d0d8507b0d724d1e21400dfe5712
jepthe Nkwanmen <107397519+nosby2007@users.noreply.github.com>
2026-07-17 13:59:05 -0400

    Deploy scoped checkout reservations

--- functions/main.js
+++ functions/main.js
@@ -1,9 +1,11 @@
 'use strict';
 
 const marketplace = require('./marketplace-v3');
+const checkout = require('./checkout-v4');
 const roleApproval = require('./role-approval');
 
 module.exports = {
   ...marketplace,
+  ...checkout,
   approveRoleRequest: roleApproval.approveRoleRequest
 };
