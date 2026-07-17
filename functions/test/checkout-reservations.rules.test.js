3d4aa9896cfa6d16089c695fe91a6cad2d6e6828
jepthe Nkwanmen <107397519+nosby2007@users.noreply.github.com>
2026-07-17 13:59:50 -0400

    Test checkout reservation privacy rules

---
+++ functions/test/checkout-reservations.rules.test.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const { readFileSync } = require('node:fs');
+const path = require('node:path');
+const { test, before, after } = require('node:test');
+const {
+  initializeTestEnvironment,
+  assertFails
+} = require('@firebase/rules-unit-testing');
+const { ref, get, set } = require('firebase/database');
+
+let testEnv;
+
+before(async () => {
+  testEnv = await initializeTestEnvironment({
+    projectId: 'lamylenoise-checkout-rules-test',
+    database: {
+      rules: readFileSync(path.join(__dirname, '../../database.rules.json'), 'utf8')
+    }
+  });
+});
+
+after(async () => {
+  await testEnv.cleanup();
+});
+
+test('checkout reservation recovery records are server-only', async () => {
+  const db = testEnv.authenticatedContext('customer-1').database();
+  await assertFails(get(ref(db, 'checkoutReservations/order-1')));
+  await assertFails(set(ref(db, 'checkoutReservations/order-1'), {
+    customerUid: 'customer-1',
+    tenantId: 'lamylenoise',
+    expiresAt: Date.now() + 60000
+  }));
+});
